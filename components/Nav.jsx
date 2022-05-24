cb19ed55109c8b5025cd75b79065d881c3735895
Neeraj Dalal <admin@nrjdalal.com>
2022-05-24 16:32:40 +0530

    temp nav

---
+++ components/Nav.jsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+
+const Nav = () => {
+  return (
+    <div className="flex space-x-2">
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/market">
+        <a>Market</a>
+      </Link>
+      <Link href="/collections">
+        <a>Collections</a>
+      </Link>
+      <Link href="/stats">
+        <a>Stats</a>
+      </Link>
+      <Link href="/faq">
+        <a>FAQ</a>
+      </Link>
+      <Link href="/create">
+        <a>Create</a>
+      </Link>
+    </div>
+  )
+}
+
+export default Nav
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,11 @@
 import '../styles/globals.css'
+import Nav from 'components/Nav'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <div className="relative h-screen w-screen overflow-x-hidden bg-slate-900 text-white">
+        <Nav />
         <Component {...pageProps} />
       </div>
     </>
