a0de2eed6017010013571f4b89f4539316424de7
Neeraj Dalal <admin@nrjdalal.com>
2022-05-24 19:49:07 +0530

    stats added

--- pages/stats.jsx
+++ pages/stats.jsx
@@ -35,29 +35,45 @@ const Stats = () => {
       <div className="my-10 flex flex-col items-center">
         <h1 className="text-2xl font-bold text-emerald-500">Statistics for all collections</h1>
 
+        {
+          // ~ Stats for ETC
+        }
         <div className="mt-10 flex w-full flex-col items-center justify-center rounded-xl bg-white p-4 font-medium text-emerald-500 sm:w-auto sm:flex-row">
-          <div className="flex w-full flex-col items-center p-4 sm:w-48">
+          <div className="flex w-full flex-col items-center p-5 sm:w-48">
             <h2>ETC Market Cap</h2>
+            <div className={`mt-1 rounded-xl px-2 py-1 text-white ${marketChange < 0 ? 'bg-red-500' : 'bg-green-500'}`}>
+              {marketChange}%
+            </div>
             <div className="mt-2 flex items-center text-black">
               <span className="text-xl">$</span>
               <p className="pt-0.5 pl-0.5">{market}</p>
             </div>
           </div>
-          <div className="flex w-full flex-col items-center border-t-2 p-4 sm:w-48 sm:border-l-2 sm:border-t-0">
+          <div className="flex w-full flex-col items-center border-t-2 p-5 sm:w-48 sm:border-l-2 sm:border-t-0">
             <h2>ETC Price</h2>
+            <div className={`mt-1 rounded-xl px-2 py-1 text-white ${priceChange < 0 ? 'bg-red-500' : 'bg-green-500'}`}>
+              {priceChange}%
+            </div>
             <div className="mt-2 flex items-center text-black">
               <span className="text-xl">$</span>
               <p className="pt-0.5 pl-0.5">{price}</p>
             </div>
           </div>
-          <div className="flex w-full flex-col items-center border-t-2 p-4 sm:w-48 sm:border-l-2 sm:border-t-0">
+          <div className="flex w-full flex-col items-center border-t-2 p-5 sm:w-48 sm:border-l-2 sm:border-t-0">
             <h2>ETC 24h Volume</h2>
+            <div className={`mt-1 rounded-xl px-2 py-1 text-white ${volumeChange < 0 ? 'bg-red-500' : 'bg-green-500'}`}>
+              {volumeChange}%
+            </div>
             <div className="mt-2 flex items-center text-black">
               <span className="text-xl">$</span>
               <p className="pt-0.5 pl-0.5">{volume}</p>
             </div>
           </div>
         </div>
+
+        {
+          // ~ Stats for collections
+        }
       </div>
     </div>
   )
