1a9983dd68763546185c166f2e4052c2e136ace1
Neeraj Dalal <admin@nrjdalal.com>
2022-05-24 23:56:45 +0530

    home updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,7 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [
-    '@tailwindcss/forms',
-    '@tailwindcss/line-clamp',
-    '@tailwindcss/typography',
-  ],
+  plugins: ['@tailwindcss/aspect-ratio', '@tailwindcss/forms', '@tailwindcss/line-clamp', '@tailwindcss/typography'],
 }
