f5528ad9cb2fcc54a00053c92b9f9a864bad7b1c
Neeraj Dalal <admin@nrjdalal.com>
2022-05-24 17:04:17 +0530

    faq added

--- pages/faq.jsx
+++ pages/faq.jsx
@@ -6,12 +6,37 @@ const faq = () => {
 
     return (
       <div
-        className="mt-10 w-full max-w-3xl cursor-pointer rounded-xl bg-teal-900 p-4"
+        className="relative mt-10 w-full max-w-3xl cursor-pointer rounded-xl bg-teal-900 p-4"
         key={id}
         onClick={() => setOpen(!open)}
       >
+        <div className="absolute top-4 right-5">
+          {open ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M5 15l7-7 7 7" />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+            </svg>
+          )}
+        </div>
         <h2 className="text-xl font-semibold text-emerald-500">{title}</h2>
-        <p className={`pt-3 text-justify ${open ? 'block' : 'hidden'}`}>{content}</p>
+        <p className={`pt-3 ${open ? 'block' : 'hidden'}`}>{content}</p>
       </div>
     )
   }
