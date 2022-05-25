be70352c7957c4eedfe1a795e026a5a0e7e79d6f
Neeraj Dalal <admin@nrjdalal.com>
2022-05-25 17:30:38 +0530

    home updated

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,7 +4,7 @@ import Nav from 'components/Nav'
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <div className="relative h-screen w-screen overflow-x-hidden bg-slate-900 font-dmSans leading-6 text-white">
+      <div className="relative h-screen w-screen overflow-x-hidden bg-slate-900 font-dmSans leading-6 tracking-wide text-white">
         <Nav />
         <Component {...pageProps} />
       </div>
